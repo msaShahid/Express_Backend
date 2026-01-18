f083e00a7979e10c67b70f5fe2088d6a1bfa18b6
Mohammad Shahid <msa.ansari28@gmail.com>
2026-01-18 15:20:44 +0530

    `feat(auth - middleware): add JWT authentication middleware to protect routes Verifies access token from headers Attaches user info to request object Handles unauthorized and invalid tokens`

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+  user?: {
+    userId: string;
+    role: "USER" | "ADMIN";
+  };
+}
+
+export function requireAuth(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const header = req.headers.authorization;
+  if (!header?.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token" });
+  }
+
+  try {
+    const token = header.split(" ")[1];
+
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as {
+      userId: string;
+      role: "USER" | "ADMIN";
+    };
+
+    req.user = {
+      userId: payload.userId,
+      role: payload.role,
+    };
+
+    next();
+  } catch (err: any) {
+    console.error("JWT verification error:", err.message); 
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
