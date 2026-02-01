9acb2e255207dc6451011fbb41c5b095d197ef30
Mohammad Shahid <msa.ansari28@gmail.com>
2026-02-01 23:32:56 +0530

    `feat(db): add user factory - Add userFactory for generating UserCreateInput with Faker`

--- prisma/factories/user.factory.ts
+++ prisma/factories/user.factory.ts
@@ -1,14 +1,14 @@
-import { Prisma, Role } from '../../generated/prisma'
+import { Prisma, Role, UserStatus } from '../../generated/prisma'
 import { faker } from '@faker-js/faker'
 
 export const userFactory = (
   overrides?: Partial<Prisma.UserCreateInput>
 ): Prisma.UserCreateInput => ({
   name: faker.person.fullName(),
-  email: faker.internet.email().toLowerCase(),
+  email: faker.internet.email({ allowSpecialCharacters: false }).toLowerCase(),
   password: faker.internet.password(),
   role: Role.USER,
   emailVerified: true,
-  isActive: true,
+  status: UserStatus.ACTIVE,
   ...overrides,
 })
